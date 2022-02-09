db363283d3e4bb0c7f7f7aebdf2a08cb0d659b23
terlam <max.rel@gmail.com>
2022-02-09 09:43:07 -0600

    user model, forms & db linking

--- drone_inventory/__init__.py
+++ drone_inventory/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from config import Config
 from .site.routes import site
 from .api.routes import api
 from .authentication.routes import auth
+from .models import db as root_db
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
@@ -10,3 +15,6 @@ app.config.from_object(Config)
 app.register_blueprint(site)
 app.register_blueprint(auth)
 app.register_blueprint(api)
+
+root_db.init_app(app)
+migrate = Migrate(app,root_db)
